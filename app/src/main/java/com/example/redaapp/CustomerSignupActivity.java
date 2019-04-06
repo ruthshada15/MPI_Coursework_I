5fc788d814076d5a8ecec27a9e1ea4592bc81d4a
ruthshada15 <ruthshada15@gmail.com>
2019-04-06 12:49:48 +0300

    Storing customer information in Database Working without profile picture

--- app/src/main/java/com/example/redaapp/CustomerSignupActivity.java
+++ app/src/main/java/com/example/redaapp/CustomerSignupActivity.java
@@ -16,11 +16,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class CustomerSignupActivity extends AppCompatActivity {
-    private EditText stremail, strpassword;
+    private EditText stremail, strpassword, strfname, strlname, strphonenum, strconfirmpass;
     private Button getstarted;
 
     private ProgressDialog loadingBar;
@@ -37,10 +38,15 @@ public class CustomerSignupActivity extends AppCompatActivity {
 
         myAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         loadingBar = new ProgressDialog(this);
 
         stremail = (EditText) findViewById(R.id.signuptxtemail);
         strpassword = (EditText) findViewById(R.id.signuptxtpassword);
+        strfname = (EditText) findViewById(R.id.txtcustomerfname);
+        strlname = (EditText) findViewById(R.id.txtcustomerlname);
+        strphonenum = (EditText) findViewById(R.id.txtcustomerphonenumber);
+        strconfirmpass = (EditText) findViewById(R.id.txtcustomerconfirmpassword);
         getstarted = (Button) findViewById(R.id.btngetstarted);
 
         getstarted.setOnClickListener(new View.OnClickListener() {
@@ -49,9 +55,12 @@ public class CustomerSignupActivity extends AppCompatActivity {
 
                 String email = stremail.getText().toString().trim();
                 String password = strpassword.getText().toString().trim();
+                String fname = strfname.getText().toString().trim();
+                String lname = strlname.getText().toString().trim();
+                String phonenum = strphonenum.getText().toString().trim();
+                String confirmpass = strconfirmpass.getText().toString().trim();
 
-                validation(email,password);
-
+                validation(email,password,fname,lname,phonenum,confirmpass);
 
 
             }
@@ -59,25 +68,54 @@ public class CustomerSignupActivity extends AppCompatActivity {
     }
 
 
-
-    private void validation(String a, String b) {
+    private void validation(String a, String b, String c, String d, String e, String f) {
         if (TextUtils.isEmpty(a)) {
             stremail.setError("Enter your Email Adress");
             stremail.requestFocus();
             return;
         }
+        if (TextUtils.isEmpty(c)) {
+            strfname.setError("Enter your First Name");
+            strfname.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(d)) {
+            strlname.setError("Enter your Last Name");
+            strlname.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(e)) {
+            strphonenum.setError("Enter your Phone Number");
+            strphonenum.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(f)) {
+            strconfirmpass.setError("You need to confirm your password");
+            strconfirmpass.requestFocus();
+            return;
+        }
+
         if (!Patterns.EMAIL_ADDRESS.matcher((CharSequence) a).matches()) {
             stremail.setError("Enter a Valid* Email Adress");
             stremail.requestFocus();
             return;
         }
+
         if (TextUtils.isEmpty(b)) {
-            strpassword.setError("Enter your Password");
+                strpassword.setError("Enter your Password");
+                strpassword.requestFocus();
+                return;
+        }
+
+        if (b.length() < 6) {
+            strpassword.setError("Password is too short");
             strpassword.requestFocus();
             return;
         }
-        if (b.length() < 6) {
-            strpassword.setError("Password is too short");
+        if (!b.equals(f)) {
+            strconfirmpass.setError("Passwords don't match");
+            strpassword.setError("Passwords don't match");
+            strconfirmpass.requestFocus();
             strpassword.requestFocus();
             return;
         }
@@ -97,6 +135,8 @@ public class CustomerSignupActivity extends AppCompatActivity {
                         // Toast.makeText(getApplicationContext(), id, Toast.LENGTH_SHORT).show();
                         loadingBar.dismiss();
 
+                            saveCustomerInformation();
+
                         startActivity(new Intent(getApplicationContext(), MapsActivity.class));
                     } else {
                         // Toast.makeText(getApplicationContext(), "nooo", Toast.LENGTH_SHORT).show();
@@ -108,4 +148,18 @@ public class CustomerSignupActivity extends AppCompatActivity {
         }
 
     }
+
+    private void saveCustomerInformation (){
+
+        String fname = strfname.getText().toString().trim();
+        String lname = strlname.getText().toString().trim();
+        String phonenum = strphonenum.getText().toString().trim();
+
+        CustomerInformation customerInformation = new CustomerInformation(fname,lname,phonenum);
+        FirebaseUser user = myAuth.getCurrentUser();
+        databaseReference.child(user.getUid()).setValue(customerInformation);
+
+    }
+
+
 }
